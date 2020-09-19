c7b5e8444e791d16514fededba966e52a855c8e9
SAJIB ROY <sajibroy206@gmail.com>
2020-09-19 14:08:48 +0600

    first_second

--- app/src/main/java/com/example/walpaperapp/FullScreenActivity.java
+++ app/src/main/java/com/example/walpaperapp/FullScreenActivity.java
@@ -2,6 +2,7 @@ package com.example.walpaperapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DownloadManager;
 import android.app.WallpaperManager;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -22,7 +23,7 @@ public class FullScreenActivity extends AppCompatActivity {
 
     private PhotoView photoView;
     private String URi;
-    private MaterialButton set_walpaper;
+    private MaterialButton set_walpaper, downloadwalpaper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +35,7 @@ public class FullScreenActivity extends AppCompatActivity {
         URi = intent.getStringExtra(DataManager.ClickImageUri);
         getSupportActionBar().hide();
 
+        downloadwalpaper = findViewById(R.id.DownloadWalpaper);
         Picasso.with(getApplicationContext()).load(URi).into(photoView);
 
         set_walpaper = findViewById(R.id.SetupWalperButon);
@@ -52,6 +54,21 @@ public class FullScreenActivity extends AppCompatActivity {
                 }
 
 
+            }
+        });
+
+
+        downloadwalpaper.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                DownloadManager downloadManager = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+                Uri uri = Uri.parse(URi);
+                DownloadManager.Request request = new DownloadManager.Request(uri);
+                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+                downloadManager.enqueue(request);
+
+                Toast.makeText(getApplicationContext(), "Downloading ...", Toast.LENGTH_SHORT).show();
             }
         });
     }
